b5d1da0dea3edc1f7e93d1e4ad52667baf5576dc
Gean <gean.marroquin@gmail.com>
2016-07-25 11:22:39 -0500

    [update] delete dist folder on build

--- webpack/build.js
+++ webpack/build.js
@@ -4,12 +4,18 @@ process.env.NODE_ENV = 'production'
 
 import webpack from 'webpack'
 import ora from 'ora'
+import path from 'path'
+import {rm, mkdir} from 'shelljs'
 
 import prodWebpackConfig from './webpack.conf.prod.babel'
 
 let spinner = ora('Building for production...')
 
 spinner.start()
+
+const distFolderPath = path.resolve(__dirname, '../dist')
+rm('-rf', distFolderPath)
+mkdir('-p', distFolderPath)
 
 webpack(prodWebpackConfig).run((err, stats) => {
   spinner.stop()
