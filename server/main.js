0771eb601e35131e4821998b208123b6f1741e84
Gean <gean.marroquin@gmail.com>
2016-08-18 19:48:34 -0500

    [update] listen logs

--- server/main.js
+++ server/main.js
@@ -2,15 +2,18 @@
 
 const express = require('express')
 const path = require('path')
+const myIp = require('my-ip')
 
 const app = express()
 
 const PORT = process.env.PORT || 8080
-const HOST = process.env.HOST || '0.0.0.0'
+const CURRENT_IP = myIp()
 
 // point dist folder
 app.use('/', express.static(path.resolve(__dirname, '../dist')))
 
-app.listen(PORT, HOST, function () {
-  console.log(`Express server listening at http://${HOST}:${PORT}\n`)
+app.listen(PORT, '0.0.0.0', function () {
+  console.log('Express server listening at:')
+  console.log(`http://localhost:${PORT}`)
+  console.log(`http://${CURRENT_IP}:${PORT}\n`)
 })
