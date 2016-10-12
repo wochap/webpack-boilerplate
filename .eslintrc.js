3bd4d8c9cceb3ed6d5146b2b4e9f470fea906281
wochap <gean.marroquin@gmail.com>
2016-10-12 14:02:52 -0500

    [update] add DEBUG var

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+const DEBUG = process.env.NODE_ENV !== 'production'
+
+module.exports = {
+  'root': true,
+  'extends': [
+    'eslint:recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'standard'
+  ],
+  'settings': {
+    'import/resolver': {
+      'webpack': {
+        'config': 'build/webpack/webpack.conf.dev.babel.js'
+      }
+    }
+  },
+  'parserOptions': {
+    'sourceType': 'module',
+  },
+  rules: {
+    'no-console': DEBUG ? 0 : 2,
+    'no-debugger': DEBUG ? 0 : 2
+  }
+}
