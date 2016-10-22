bfc3ee3faf1dfb22f894c0c62d31d02bbc905552
wochap <gean.marroquin@gmail.com>
2016-10-22 11:01:37 -0500

    [feat] add HMR for main.js

--- src/app/main.js
+++ src/app/main.js
@@ -1,2 +1,8 @@
 require('src/favicon.ico')
 require('src/styles/main.scss')
+
+// This tells Webpack that this file and all of its dependencies can be replaced.
+// http://andrewhfarmer.com/webpack-hmr-tutorial/#the-simple-way
+if (module.hot) {
+  module.hot.accept()
+}
