13a4eee8fd96103537aaf4ff4f961b6ec107ccce
wochap <gean.marroquin@gmail.com>
2016-11-15 11:24:59 -0500

    add projectSourcePath variable

--- build/config.js
+++ build/config.js
@@ -5,4 +5,5 @@ export const CURRENT_IP = myIp()
 export const BROWSER_SYNC_PORT = 3000
 export const WEBPACK_SERVER_PORT = 8080
 export const projectRootPath = path.resolve(__dirname, '../')
+export const projectSourcePath = path.resolve(__dirname, '../src')
 export const templatePath = path.join(projectRootPath, 'src/index.html')
