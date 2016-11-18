af4a32187fe353e7002ec436ed891747bd8d2dba
wochap <gean.marroquin@gmail.com>
2016-11-18 14:20:19 -0500

    [style] replace require with import statement

--- src/app/main.js
+++ src/app/main.js
@@ -1,5 +1,7 @@
-require('src/favicon.ico')
-require('src/styles/main.scss')
+// favicon to keep your browser from throwing a 404 during dev
+import 'src/favicon.ico'
+
+import 'src/styles/main.scss'
 
 // This tells Webpack that this file and all of its dependencies can be replaced.
 // http://andrewhfarmer.com/webpack-hmr-tutorial/#the-simple-way
